6d9cd7bf77757b4a5fa244522867638124fe8a76
Eva <eva.krzisnik@gmail.com>
2026-03-02 12:45:18 +0100

    renamed folders to represent pipeline order up to assembly, added new ONT and old HiFi library to decontamination step

--- desiree_resequencing/2_plastid_contaminants_removal/scripts/plastid_decontamination.sh
+++ desiree_resequencing/2_plastid_contaminants_removal/scripts/plastid_decontamination.sh
@@ -45,3 +45,38 @@ output=$out_base/library2_decontaminated.paf
 minimap2 -x map-ont -t $threads $target $query > $output
 
 conda deactivate
+
+#######################################################
+#MAR26
+#korak takoj po qc odcitkov in chopperju, ze pred assemblyjem se zelimo znebiti odcitkov, ki mapirajo na organele
+
+query="/scratch/evakrzisnik/desiree_resequencing/reads_qc/outputs/chopper/library1.fixedcrop_h20_t5_q10.fastq"
+target="/scratch/evakrzisnik/desiree_resequencing/plastid_contaminants_removal/input/desiree_organelles.fa" 
+
+threads=100
+out_base=/scratch/evakrzisnik/desiree_resequencing/plastid_contaminants_removal/output
+
+conda activate minimap
+
+output=$out_base/library3_decontaminated.paf
+#preveri preden pozenes, kako bos imenovala library pri qc, 1 ali 3, in uporabljaj konsistentno
+
+minimap2 -x map-ont -t $threads $target $query > $output
+
+conda deactivate
+
+#isto bomo dekontaminirali tudi HiFi odcitke izpred mojega prihoda, ki grejo tudi v assembly
+query=""
+target="/scratch/evakrzisnik/desiree_resequencing/plastid_contaminants_removal/input/desiree_organelles.fa" 
+
+threads=100
+out_base=/scratch/evakrzisnik/desiree_resequencing/plastid_contaminants_removal/output
+
+conda activate minimap
+
+output=$out_base/desiree_hifi_decontaminated.paf
+
+#popravimo parameter, ker ne delamo vec z ont, ampak s pacbio hifi
+minimap2 -x map-hifi -t $threads $target $query > $output
+
+conda deactivate
