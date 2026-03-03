e05796cbd7b7482da3e06735ec0c978a41647a4d
Eva Krzisnik <eva.krzisnik@gmail.com>
2026-03-03 16:19:16 +0100

    fixed and used scripts for nanopack and plastid decontamination

--- desiree_resequencing/1_reads_qc/scripts/qc_reseq_ver3.sh
+++ desiree_resequencing/1_reads_qc/scripts/qc_reseq_ver3.sh
@@ -6,19 +6,18 @@ conda activate nanopack
 
 #input="/scratch/evakrzisnik/desiree_resequencing/reads_qc/outputs/chopper/library2.filtered.fastq"
 #new input=library from february sequencing, insert
-#decide whether this is going to be called library 1 or 3, august library is lib2
-input=""
 
-output_base="/scratch/evakrzisnik/desiree_resequencing/reads_qc/outputs"
+input="/scratch/evakrzisnik/desiree_resequencing/1_reads_qc/inputs/library3.fastq"
+
+output_base="/scratch/evakrzisnik/desiree_resequencing/1_reads_qc/outputs"
 
 
-
-subfolder="lib_feb_nanoplot"
+subfolder="lib3_nanoplot"
 output="$output_base"/"$subfolder"
 mkdir -p $output
 NanoPlot --fastq "$input" -o "$output"
 
-subfolder="lib_feb_nanoqc"
+subfolder="lib3_nanoqc"
 output="$output_base"/"$subfolder"
 mkdir -p $output
 nanoQC "$input" -o "$output"
@@ -27,20 +26,90 @@ nanoQC "$input" -o "$output"
 
 #input_filepath="/scratch/evakrzisnik/desiree_resequencing/reads_qc/inputs/library2.fastq"
 #replace with library fastq file from february sequencing
-input_filepath=""
+#is a softlink from dorado outputs
+input_filepath="/scratch/evakrzisnik/desiree_resequencing/1_reads_qc/inputs/library3.fastq"
 
 #output_base="/scratch/evakrzisnik/desiree_resequencing/reads_qc/outputs"
 # Step 1: fixed crop
+#  chopper \
+#    --trim-approach fixed-crop \
+#    --headcrop 20 \
+#    --tailcrop 5 \
+#    -t 8 \
+#    -i $input_filepath \
+#  | chopper \
+#    --trim-approach -quality \
+#    --cutoff 10 \
+#    -t 8 \
+#  > /scratch/evakrzisnik/desiree_resequencing/1_reads_qc/outputs/chopper/library3.fixedcrop_h20_t5_q10.fastq
+
+
+#odrezemo prvih 20 in zadnjih 5 nt, poleg tega pa obdrzimo samo tiste odcitke z average q score 10 ali vec
 chopper \
   --trim-approach fixed-crop \
   --headcrop 20 \
   --tailcrop 5 \
+  -q 10 \
   -t 8 \
-  -i $input_filepath \
-| chopper \
-  --trim-approach quality \
-  --cutoff 10 \
+  -i "$input_filepath" \
+ > /scratch/evakrzisnik/desiree_resequencing/1_reads_qc/outputs/chopper/library3.fixedcrop_h20_t5_q10.fastq
+
+
+input="/scratch/evakrzisnik/desiree_resequencing/1_reads_qc/outputs/chopper/library3.fixedcrop_h20_t5_q10.fastq"
+
+output_base="/scratch/evakrzisnik/desiree_resequencing/1_reads_qc/outputs"
+
+
+subfolder="lib3_chopped_nanoplot"
+output="$output_base"/"$subfolder"
+mkdir -p $output
+NanoPlot --fastq "$input" -o "$output"
+
+subfolder="lib3_chopped_nanoqc"
+output="$output_base"/"$subfolder"
+mkdir -p $output
+nanoQC "$input" -o "$output"
+
+
+#hotela se preverit za tisto lib2, ki jo je choppal tim
+input=/scratch/evakrzisnik/desiree_resequencing/1_reads_qc/outputs/chopper/library2.filtered.fastq
+output_base="/scratch/evakrzisnik/desiree_resequencing/1_reads_qc/outputs"
+
+
+subfolder="lib2_chopped_nanoplot"
+output="$output_base"/"$subfolder"
+mkdir -p $output
+NanoPlot --fastq "$input" -o "$output"
+
+subfolder="lib2_chopped_nanoqc"
+output="$output_base"/"$subfolder"
+mkdir -p $output
+nanoQC "$input" -o "$output"
+
+
+#ta old reads timg library se cez headcrop, 35 head in 15 tail. trenutna verzija je samo q10, viden je deterioration kvalitete readov !!
+input_filepath=/scratch/timg/desiree_seq/input/ont/desiree_ont_raw.fastq
+
+chopper \
+  --trim-approach fixed-crop \
+  --headcrop 35 \
+  --tailcrop 15 \
   -t 8 \
-> /scratch/evakrzisnik/desiree_resequencing/reads_qc/outputs/chopper/library1.fixedcrop_h20_t5_q10.fastq
+  -i "$input_filepath" \
+ > /scratch/evakrzisnik/desiree_resequencing/1_reads_qc/outputs/chopper/timg_ont.fixedcrop_h35_t15_q10.fastq
+
+input=/scratch/evakrzisnik/desiree_resequencing/1_reads_qc/outputs/chopper/timg_ont.fixedcrop_h35_t15_q10.fastq
+output_base="/scratch/evakrzisnik/desiree_resequencing/1_reads_qc/outputs"
+
+subfolder="old_ont_h35t15q10_nanoplot"
+output="$output_base"/"$subfolder"
+mkdir -p $output
+NanoPlot --fastq "$input" -o "$output"
+
+subfolder="old_ont_h35t15q10_nanoqc"
+output="$output_base"/"$subfolder"
+mkdir -p $output
+nanoQC "$input" -o "$output"
+
 
 conda deactivate
--- desiree_resequencing/2_plastid_contaminants_removal/scripts/plastid_decontamination.sh
+++ desiree_resequencing/2_plastid_contaminants_removal/scripts/plastid_decontamination.sh
@@ -18,8 +18,8 @@
 
 #variables
 threads=100
-out_base=/scratch/evakrzisnik/desiree_resequencing/plastid_contaminants_removal/output
-
+out_base="/scratch/evakrzisnik/desiree_resequencing/2_plastid_contaminants_removal/output"
+input_base="/scratch/evakrzisnik/desiree_resequencing/2_plastid_contaminants_removal/input"
 
 
 conda activate minimap
@@ -50,33 +50,80 @@ conda deactivate
 #MAR26
 #korak takoj po qc odcitkov in chopperju, ze pred assemblyjem se zelimo znebiti odcitkov, ki mapirajo na organele
 
-query="/scratch/evakrzisnik/desiree_resequencing/reads_qc/outputs/chopper/library1.fixedcrop_h20_t5_q10.fastq"
-target="/scratch/evakrzisnik/desiree_resequencing/plastid_contaminants_removal/input/desiree_organelles.fa" 
-
+#variables
 threads=100
-out_base=/scratch/evakrzisnik/desiree_resequencing/plastid_contaminants_removal/output
+out_base="/scratch/evakrzisnik/desiree_resequencing/2_plastid_contaminants_removal/output"
+input_base="/scratch/evakrzisnik/desiree_resequencing/2_plastid_contaminants_removal/input"
+
 
 conda activate minimap
 
+query=$input_base/library3_filtered.fastq
+target=$input_base/desiree_organelles.fa
+
+
 output=$out_base/library3_decontaminated.paf
-#preveri preden pozenes, kako bos imenovala library pri qc, 1 ali 3, in uporabljaj konsistentno
+
 
 minimap2 -x map-ont -t $threads $target $query > $output
 
-conda deactivate
+
+#enako z starimi odcitki desiree od Tima
+#variables
+threads=100
+out_base="/scratch/evakrzisnik/desiree_resequencing/2_plastid_contaminants_removal/output"
+input_base="/scratch/evakrzisnik/desiree_resequencing/2_plastid_contaminants_removal/input"
+
+query=$input_base/timg_ont.fastq
+target=$input_base/desiree_organelles.fa
+
+output=$out_base/timg_ont_decontaminated.paf
+
+
+minimap2 -x map-ont -t $threads $target $query > $output
 
 #isto bomo dekontaminirali tudi HiFi odcitke izpred mojega prihoda, ki grejo tudi v assembly
-query=""
-target="/scratch/evakrzisnik/desiree_resequencing/plastid_contaminants_removal/input/desiree_organelles.fa" 
 
 threads=100
-out_base=/scratch/evakrzisnik/desiree_resequencing/plastid_contaminants_removal/output
+out_base=/scratch/evakrzisnik/desiree_resequencing/2_plastid_contaminants_removal/output
+input_base="/scratch/evakrzisnik/desiree_resequencing/2_plastid_contaminants_removal/input"
 
-conda activate minimap
+query=$input_base/timg_hifi.fastq
+target=$input_base/desiree_organelles.fa
 
-output=$out_base/desiree_hifi_decontaminated.paf
+output=$out_base/timg_hifi_decontaminated.paf
 
 #popravimo parameter, ker ne delamo vec z ont, ampak s pacbio hifi
+
 minimap2 -x map-hifi -t $threads $target $query > $output
 
 conda deactivate
+
+#zdaj se preselimo v okolje R
+#.paf datoteke, ki smo jih pridobili, bomo nesli naprej in jih analizirali
+#.paf datoteke si scp-jam na komp, od koder bomo delali naprej 
+#vrnemo se sem z dolocenim thresholdom za filtriranje in filtriranje samo
+conda create -n seqkit -c bioconda -c conda-forge seqkit
+
+#za hifi se kar odlocim za cutoff, to naredim in se enkrat zmapiram in plotam
+input_base=/scratch/evakrzisnik/desiree_resequencing/2_plastid_contaminants_removal/input
+out_base=/scratch/evakrzisnik/desiree_resequencing/2_plastid_contaminants_removal/output
+
+original_hifi=$input_base/timg_hifi.fastq
+
+conda activate seqkit
+
+seqkit grep -v -f $input_base/hifi_contaminant_ids.txt $original_hifi > $out_base/hifi.cleaned.fastq
+
+#sanity check, should contain fewer reads after running
+seqkit stats $original_hifi
+seqkit stats $out_base/hifi.cleaned.fastq
+
+conda deactivate
+
+#output za hifi:
+# [INFO] 200116 patterns loaded from file
+# file                                                                                            format  type   num_seqs         sum_len  min_len   avg_len  max_len
+# /scratch/evakrzisnik/desiree_resequencing/2_plastid_contaminants_removal/input/timg_hifi.fastq  FASTQ   DNA   4,124,274  79,351,821,101      139  19,240.2   50,124
+# file                                                                                                format  type   num_seqs         sum_len  min_len   avg_len  max_len
+# /scratch/evakrzisnik/desiree_resequencing/2_plastid_contaminants_removal/output/hifi.cleaned.fastq  FASTQ   DNA   3,924,158  75,523,358,034      139  19,245.7   50,124
